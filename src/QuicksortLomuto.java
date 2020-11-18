7fd7553d5a1c2b27b944a25422f1c57a8166144b
BramUt <bram.utberg@gmail.com>
2020-11-18 12:39:19 +0100

    Done

--- src/QuicksortLomuto.java
+++ src/QuicksortLomuto.java
@@ -2,20 +2,24 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
 
+
 public class QuicksortLomuto {
 
     public static void main(String[] args) {
-        int n = 100;
+        int n = 1_000_000;
 
-        ArrayList<Integer> list = new ArrayList<Integer>();
+        ArrayList<Integer> list = new ArrayList<>();
         Random rd = new Random();
 
         for (int i = 0; i < n; i++) {
             list.add(rd.nextInt());
         }
         System.out.println(list.toString());
+        long startTime = System.currentTimeMillis();
         quicksort(list, 0, list.size() - 1);
         System.out.println(list.toString());
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime-startTime + " milliseconds");
     }
 
 
